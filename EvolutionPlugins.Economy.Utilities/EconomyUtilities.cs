9e1d465a6e39bb16db8464f61b143dd35376af7b
DiFFoZ <48765566+DiFFoZ@users.noreply.github.com>
2021-08-13 22:46:57 +0700

    Add project files.

---
+++ EvolutionPlugins.Economy.Utilities/EconomyUtilities.cs
@@ -0,0 +1,16 @@
+﻿using OpenMod.API.Plugins;
+using OpenMod.Unturned.Plugins;
+using System;
+
+[assembly: PluginMetadata("EvolutionPlugins.Economy.Utilities", DisplayName = "Economy Utilities", Website = "https://discord.gg/5MT2yke")]
+
+namespace EvolutionPlugins.Economy.Utilities
+{
+    public class EconomyUtilities : OpenModUnturnedPlugin
+    {
+
+        public EconomyUtilities(IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+        }
+    }
+}
